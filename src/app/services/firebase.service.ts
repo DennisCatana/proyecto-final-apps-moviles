7bb69f40af14e0c8fddac112859b6bb61f142040
DennisCatana <dennis.joel235@outlook.es>
2024-07-28 19:44:34 -0500

    navegación lista

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, up
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ export class FirebaseService {
 
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
+  utilsSvc = inject(UtilsService);
 
   //================================ Autenticación =====
 
@@ -35,6 +37,13 @@ export class FirebaseService {
   //===== Recuperar password =====
   sendRecoveryEmail(email: string) {
     return sendPasswordResetEmail(getAuth(), email);
+  }
+
+  //===== Cerrar sesion =====
+  signOut() {
+    getAuth().signOut();
+    localStorage.removeItem('user');
+    this.utilsSvc.routerLink('/auth');
   }
 
   //==================== Base de datos =============================
