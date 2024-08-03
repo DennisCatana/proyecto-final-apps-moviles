af663a121a1e04ac8e4a2a0c468aee72b51ea496
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-08-03 01:16:48 -0500

    Arreglo de errores

--- src/app/pages/main/ubicacion/ubicacion.page.ts
+++ src/app/pages/main/ubicacion/ubicacion.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { GeolocationService } from 'src/app/services/geolocation.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Geolocation, PositionOptions, GeolocationPosition } from '@capacitor/geolocation';
+import { Geolocation, GeolocationPosition, PositionOptions } from '@capacitor/geolocation';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -65,10 +65,7 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
 
         if (this.locationMarkers[location.uid]) {
           this.locationMarkers[location.uid].setPosition(new google.maps.LatLng(location.lat, location.lng));
-          const infowindow = new google.maps.InfoWindow({
-            content: infoContent
-          });
-          infowindow.open(this.map, this.locationMarkers[location.uid]);
+          this.addInfoWindow(this.locationMarkers[location.uid], infoContent);
         } else {
           const marker = new google.maps.Marker({
             position: new google.maps.LatLng(location.lat, location.lng),
@@ -79,10 +76,7 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
             }
           });
 
-          const infowindow = new google.maps.InfoWindow({
-            content: infoContent
-          });
-          infowindow.open(this.map, marker);
+          this.addInfoWindow(marker, infoContent);
 
           this.locationMarkers[location.uid] = marker;
         }
@@ -104,10 +98,7 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
 
         if (this.markerMarkers[markerData.uid]) {
           this.markerMarkers[markerData.uid].setPosition(new google.maps.LatLng(markerData.lat, markerData.lng));
-          const infowindow = new google.maps.InfoWindow({
-            content: infoContent
-          });
-          infowindow.open(this.map, this.markerMarkers[markerData.uid]);
+          this.addInfoWindow(this.markerMarkers[markerData.uid], infoContent);
         } else {
           const marker = new google.maps.Marker({
             position: new google.maps.LatLng(markerData.lat, markerData.lng),
@@ -118,10 +109,7 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
             }
           });
 
-          const infowindow = new google.maps.InfoWindow({
-            content: infoContent
-          });
-          infowindow.open(this.map, marker);
+          this.addInfoWindow(marker, infoContent);
 
           this.markerMarkers[markerData.uid] = marker;
         }
@@ -130,26 +118,25 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
   }
 
   startAutoUpdateLocation() {
-    const options: PositionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0
-    };
-
-    this.watchId = Geolocation.watchPosition(options, (position, err) => {
-      if (position) {
-        this.updateLocation(position);
-      } else {
-        console.error('Error watching position', err);
-      }
-    });
+    this.updateInterval = setInterval(() => {
+      this.auth.currentUser.then(user => {
+        if (user) {
+          Geolocation.getCurrentPosition({ enableHighAccuracy: true }).then(position => {
+            const { latitude, longitude } = position.coords;
+            this.geolocationService.updateLocation(user.uid, latitude, longitude, user.displayName);
+          }).catch(error => {
+            console.error('Error getting current position', error);
+          });
+        }
+      });
+    }, 10000); // Update every 10 seconds
   }
 
   async updateLocation(position: GeolocationPosition) {
     const user = await this.auth.currentUser;
     if (user) {
       const { latitude, longitude, accuracy } = position.coords;
-      if (accuracy < 50) {
+      if (accuracy < 50) { // Only update if accuracy is less than 50 meters
         this.geolocationService.updateLocation(user.uid, latitude, longitude, user.displayName);
 
         if (this.firstLoad) {
@@ -171,10 +158,7 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
             }
           });
 
-          const infowindow = new google.maps.InfoWindow({
-            content: `${user.displayName} (Ubicación en tiempo real)`
-          });
-          infowindow.open(this.map, this.userMarker);
+          this.addInfoWindow(this.userMarker, `${user.displayName} (Ubicación en tiempo real)`);
         }
       }
     }
@@ -186,32 +170,15 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
       try {
         const position = await this.getCurrentPosition();
 
-        const marker = new google.maps.Marker({
-          position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-          map: this.map,
-          icon: {
-            url: './assets/icon/marcador.png',
-            scaledSize: new google.maps.Size(30, 30)
-          }
-        });
-
-        const infowindow = new google.maps.InfoWindow({
-          content: `${user.displayName} (Marcador)`
-        });
-        infowindow.open(this.map, marker);
-
-        this.markerMarkers[user.uid] = marker;
-
-        this.geolocationService.addMarker(user.uid, position.coords.latitude, position.coords.longitude, user.displayName);
-      } catch (error) {
-        console.error('Error adding marker', error);
-        alert('Error adding marker: ' + error.message);
-
-        // Use the last known position if available
-        const lastPosition = await this.getLastKnownPosition();
-        if (lastPosition) {
+        // Check if the user already has a marker
+        if (this.markerMarkers[user.uid]) {
+          // Update the existing marker position
+          this.markerMarkers[user.uid].setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+          this.geolocationService.addMarker(user.uid, position.coords.latitude, position.coords.longitude, user.displayName); // Changed here
+        } else {
+          // Add a new marker
           const marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lastPosition.coords.latitude, lastPosition.coords.longitude),
+            position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
             map: this.map,
             icon: {
               url: './assets/icon/marcador.png',
@@ -219,47 +186,46 @@ export class UbicacionPage implements OnInit, AfterViewInit, OnDestroy {
             }
           });
 
-          const infowindow = new google.maps.InfoWindow({
-            content: `${user.displayName} (Marcador)`
-          });
-          infowindow.open(this.map, marker);
+          this.addInfoWindow(marker, `${user.displayName} (Marcador)`);
 
           this.markerMarkers[user.uid] = marker;
 
-          this.geolocationService.addMarker(user.uid, lastPosition.coords.latitude, lastPosition.coords.longitude, user.displayName);
+          this.geolocationService.addMarker(user.uid, position.coords.latitude, position.coords.longitude, user.displayName);
         }
-      }
-    }
-  }
-
-  async getCurrentPosition(retries: number = 3): Promise<GeolocationPosition> {
-    const options: PositionOptions = {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0
-    };
-
-    for (let i = 0; i < retries; i++) {
-      try {
-        return await Geolocation.getCurrentPosition(options);
       } catch (error) {
-        if (i === retries - 1) {
-          console.error('Error getting current position', error);
-          alert('Error getting current position: ' + error.message);
-          throw error;
-        }
+        console.error('Error adding marker', error);
+        alert('Error adding marker: ' + error.message);
       }
     }
-
-    throw new Error('Failed to get current position after multiple attempts');
   }
 
-  async getLastKnownPosition(): Promise<GeolocationPosition | null> {
+  async getCurrentPosition(): Promise<GeolocationPosition> {
     try {
-      return await Geolocation.getCurrentPosition({ maximumAge: Infinity });
+      return await Geolocation.getCurrentPosition({ enableHighAccuracy: true, timeout: 5000 });
     } catch (error) {
-      console.error('Error getting last known position', error);
-      return null;
+      console.error('Error getting current position', error);
+      throw error;
     }
+  }
+
+  async centerMapOnCurrentLocation() {
+    try {
+      const position = await this.getCurrentPosition();
+      this.updateLocation(position);
+      this.map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+      this.map.setZoom(15);
+    } catch (error) {
+      console.error('Error centering map on current location', error);
+    }
+  }
+
+  addInfoWindow(marker: google.maps.Marker, content: string) {
+    const infoWindow = new google.maps.InfoWindow({
+      content
+    });
+
+    marker.addListener('click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 }
