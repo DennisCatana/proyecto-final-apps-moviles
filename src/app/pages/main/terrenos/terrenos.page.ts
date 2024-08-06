c7b5ac586f145dbd5cc452861facc3dcc65cd961
DannyYanacallo1755 <danny9yanacallo@gmail.com>
2024-08-06 11:21:24 -0500

    area mapa

--- src/app/pages/main/terrenos/terrenos.page.ts
+++ src/app/pages/main/terrenos/terrenos.page.ts
@@ -1,92 +1,89 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { GeolocationService } from 'src/app/services/geolocation.service';
+import { Subscription } from 'rxjs';
+
+declare const google: any;
 
 @Component({
   selector: 'app-terrenos',
   templateUrl: './terrenos.page.html',
   styleUrls: ['./terrenos.page.scss'],
 })
-export class TerrenosPage implements OnInit {
-  map!: google.maps.Map;
-  marker!: google.maps.Marker;
-  polygon: google.maps.Polygon | null = null;
-  polygonPaths: google.maps.LatLngLiteral[] = [];
-  Varea: number | null = null;
-  Vperimetro: number | null = null;
-  users$: Observable<any[]>;
-  selectedUsers: any[] = [];
-  showUserList: boolean = false;
+export class TerrenosPage implements OnInit, AfterViewInit, OnDestroy {
+  map: google.maps.Map;
+  markers: google.maps.Marker[] = [];
+  selectedMarkers: google.maps.Marker[] = [];
+  markerSubscription: Subscription;
+  polygon: google.maps.Polygon;
+  area: number = 0;
+  distances: number[] = [];
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(private geolocationService: GeolocationService) {}
 
-  ngOnInit() {
-    this.initMap();
-    this.users$ = this.firebaseService.getUsers();
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.loadMap();
+    this.trackMarkers();
   }
 
-  initMap() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const userLatLng = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          const mapOptions: google.maps.MapOptions = {
-            center: userLatLng,
-            zoom: 20,
-          };
-          this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, mapOptions);
-
-          this.marker = new google.maps.Marker({
-            position: userLatLng,
-            map: this.map,
-            title: 'Dónde estoy exactamente',
-          });
-
-          this.map.addListener('click', (event: google.maps.MapMouseEvent) => {
-            this.addLatLng(event.latLng);
-          });
-        },
-        (error) => {
-          console.error('Error obteniendo la ubicación', error);
-          this.loadDefaultMap();
-        }
-      );
-    } else {
-      console.error('Geolocalización no soportada por el navegador');
-      this.loadDefaultMap();
+  ngOnDestroy() {
+    if (this.markerSubscription) {
+      this.markerSubscription.unsubscribe();
     }
   }
 
-  loadDefaultMap() {
+  loadMap() {
     const mapOptions: google.maps.MapOptions = {
-      center: { lat: -0.187442, lng: -78.501878 },
-      zoom: 20,
+      center: new google.maps.LatLng(0, 0),
+      zoom: 8,
     };
+
     this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, mapOptions);
+  }
 
-    this.map.addListener('click', (event: google.maps.MapMouseEvent) => {
-      this.addLatLng(event.latLng);
+  trackMarkers() {
+    this.markerSubscription = this.geolocationService.getMarkers().subscribe(markers => {
+      markers.forEach(markerData => {
+        const marker = new google.maps.Marker({
+          position: new google.maps.LatLng(markerData.lat, markerData.lng),
+          map: this.map,
+          icon: {
+            url: './assets/icon/marcador.png',
+            scaledSize: new google.maps.Size(30, 30)
+          }
+        });
+
+        marker.addListener('click', () => this.selectMarker(marker));
+
+        this.markers.push(marker);
+      });
+
+      const bounds = new google.maps.LatLngBounds();
+      this.markers.forEach(marker => bounds.extend(marker.getPosition() as google.maps.LatLng));
+      this.map.fitBounds(bounds);
     });
   }
 
-  openUserSelection() {
-    this.showUserList = !this.showUserList;
-  }
+  selectMarker(marker: google.maps.Marker) {
+    if (this.selectedMarkers.includes(marker)) {
+      this.selectedMarkers = this.selectedMarkers.filter(m => m !== marker);
+      marker.setIcon({ url: './assets/icon/marcador.png', scaledSize: new google.maps.Size(30, 30) });
+    } else {
+      this.selectedMarkers.push(marker);
+      marker.setIcon({ url: './assets/icon/marcador-seleccionado.png', scaledSize: new google.maps.Size(30, 30) });
+    }
 
-  requestLocations() {
-    this.polygonPaths = [];
-    this.selectedUsers.forEach(user => {
-      this.addLatLng(new google.maps.LatLng(user.location.lat, user.location.lng));
-    });
-  }
-
-  addLatLng(latLng: google.maps.LatLng | null) {
-    if (latLng) {
-      this.polygonPaths.push(latLng.toJSON());
+    if (this.selectedMarkers.length >= 3) {
       this.drawPolygon();
+      this.calculateArea();
+      this.calculateDistances();
+    } else {
+      if (this.polygon) {
+        this.polygon.setMap(null);
+      }
+      this.area = 0;
+      this.distances = [];
     }
   }
 
@@ -95,48 +92,33 @@ export class TerrenosPage implements OnInit {
       this.polygon.setMap(null);
     }
 
+    const paths = this.selectedMarkers.map(marker => marker.getPosition() as google.maps.LatLng);
+
     this.polygon = new google.maps.Polygon({
-      paths: this.polygonPaths,
-      strokeColor: 'blue',
+      paths: paths,
+      strokeColor: '#FF0000',
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: 'red',
-      fillOpacity: 0.5,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
     });
+
     this.polygon.setMap(this.map);
   }
 
-  calculateAreaAndPerimeter() {
-    if (this.polygon) {
-      this.Varea = google.maps.geometry.spherical.computeArea(this.polygon.getPath());
-      this.Vperimetro = google.maps.geometry.spherical.computeLength(this.polygon.getPath());
-
-      console.log('Área: ', this.Varea);
-      console.log('Perímetro: ', this.Vperimetro);
-    } else {
-      console.log('No hay polígono dibujado.');
-      this.Varea = null;
-      this.Vperimetro = null;
-    }
+  calculateArea() {
+    const paths = this.polygon.getPath().getArray();
+    const area = google.maps.geometry.spherical.computeArea(paths);
+    this.area = area / 1000000; // Convert to square kilometers
   }
 
-  clearMap() {
-    if (this.polygon) {
-      this.polygon.setMap(null);
-      this.polygon = null;
-    }
-    this.polygonPaths = [];
-    this.Varea = null;
-    this.Vperimetro = null;
-    console.log('Mapa limpiado.');
-  }
-
-  toggleUserSelection(user: any) {
-    const index = this.selectedUsers.findIndex(u => u.id === user.id);
-    if (index === -1) {
-      this.selectedUsers.push(user);
-    } else {
-      this.selectedUsers.splice(index, 1);
+  calculateDistances() {
+    this.distances = [];
+    const paths = this.polygon.getPath().getArray();
+    for (let i = 0; i < paths.length; i++) {
+      const nextIndex = (i + 1) % paths.length;
+      const distance = google.maps.geometry.spherical.computeDistanceBetween(paths[i], paths[nextIndex]);
+      this.distances.push(distance / 1000); // Convert to kilometers
     }
   }
 }
